d5d6014d6945b07df5b1e37f6018df0acb135f34
sohil-hub <>
2022-05-14 21:32:18 +0530

    added Profile Model

---
+++ Social_Media_App/SocialMediaProject/core/models.py
@@ -0,0 +1,17 @@
+from email.policy import default
+from django.db import models
+from django.contrib.auth import get_user_model
+
+USER = get_user_model()
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(to=USER, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to = 'profile_images', default = 'wink.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
